82279224994d62cf5ae6a02f973f9b4e1cf25db4
Stephen <stephen.lyssy@gmail.com>
2022-10-25 22:37:09 -0500

    Added styling and added more data to the cards from the response object.

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -29,22 +29,47 @@ export default class Beers extends Component {
     // console.log('Rendered');
     return (
       <div className='page-container'>
-        <h1 className='title'>Beer List</h1>
+        <h1 className='title'>Beer Menu</h1>
         <ul>
           {this.state.beers.map((beer) => {
             console.log(beer);
             return (
-              <li>
+              <li className='details'>
                 <figure>
                   <img src={beer.image_url} alt='beer' />
                 </figure>
-                <h3>{beer.name}</h3>
+                <h3 className='beer-name'>{beer.name}</h3>
                 <h4 className='tagline'>{beer.tagline}</h4>
                 <div className='date-percentage'>
                   <p>Est. {beer.first_brewed}</p>
                   <p>Alcohol %: {beer.abv}</p>
                 </div>
                 <p className='description'>{beer.description}</p>
+                <h4 className='section-title'>Ingredients:</h4>
+                <div className='ingredients'>
+                  <ul className='ingredient-list'>
+                    <li className='ingredient-title'>Hops</li>
+                    {beer.ingredients.hops.map((hop) => {
+                      // console.log(malt.name);
+                      return <li className='hop'>{hop.name}</li>;
+                    })}
+                  </ul>
+                  <ul className='ingredient-list'>
+                    <li className='ingredient-title'>Malts</li>
+                    {beer.ingredients.malt.map((malt) => {
+                      // console.log(malt.name);
+                      return <li className='malt'>{malt.name}</li>;
+                    })}
+                  </ul>
+                  <h5 className='yeast'>Yeast: {beer.ingredients.yeast}</h5>
+                </div>
+                <h4 className='section-title'>Food Pairing</h4>
+                <p className='food-pairing'>
+                  Pairs well with{' '}
+                  {beer.food_pairing.map((food) => {
+                    return ` ${food}, `;
+                  })}
+                </p>
               </li>
             );
           })}
